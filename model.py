4e41e66dcaae806074c8690532c7dee044c8357d
MatNepo <matnepo@gmail.com>
2024-06-04 19:27:05 +0300

    model file

---
+++ model.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(28*28, 512)
+        self.fc2 = nn.Linear(512, 512)
+        self.fc3 = nn.Linear(512, 10)
+
+    def forward(self, x):
+        x = self.flatten(x)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
